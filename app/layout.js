23a7615f6adc91f85574b0f5356344a275bc4487
ludovicbeot <ludovic.beot@gmail.com>
2026-04-11 10:41:04 +0200

    maj

--- app/layout.js
+++ app/layout.js
@@ -5,19 +5,18 @@ import Header from "../components/Header";
 export const metadata = {
   title: "Claire-Lise Gaudelet",
   description: "Avocate au Barreau de Paris",
+  other: {
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
+      { href: 'https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@400;500;600;700;800;900&display=swap', rel: 'stylesheet' },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>
         <Header />
         <main className="shell">{children}</main>
